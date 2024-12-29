a84c834e5e1bc6610a095d7884ec4d8585b2c342
Dev <133972286+GreatCoder1000@users.noreply.github.com>
2024-12-29 18:05:56 +0100

    Update prss.js

--- js/prss.js
+++ js/prss.js
@@ -29,26 +29,26 @@ class PrSS {
     }
 
     eval(base = 3) {
-        if (this.sequence.length === 0) {
-            throw new Error("Cannot evaluate an empty sequence.");
-        }
-
-        if (this.sequence[this.sequence.length - 1] === 0) {
-            const s = this.sequence.slice(0, -1);
-            return new PrSS(s);
-        }
-
-        const bad = this.sequence[this.sequence.length - 1];
-        let s = [];
-        for (let i = this.sequence.length - 2; i >= 0; i--) {
-            s.unshift(this.sequence[i]);
-            if (this.sequence[i] < bad) {
-                const t = this.sequence.slice(0, i);
-                return new PrSS(t.concat(s.map(() => this.sequence[i]).concat(new Array(base + 1).fill(this.sequence[i]))));
-            }
-        }
-        return new PrSS(s.map(() => this.sequence[i]).concat(new Array(base).fill(this.sequence[i])));
+    if (this.sequence.length === 0) {
+        throw new Error("Cannot evaluate an empty sequence.");
     }
+
+    if (this.sequence[this.sequence.length - 1] === 0) {
+        const s = this.sequence.slice(0, -1);
+        return new PrSS(s);
+    }
+
+    const bad = this.sequence[this.sequence.length - 1];
+    let s = [];
+    for (let i = this.sequence.length - 2; i >= 0; i--) {
+        s.unshift(this.sequence[i]);
+        if (this.sequence[i] < bad) {
+            const t = this.sequence.slice(0, i);
+            return new PrSS(t.concat(s.concat(new Array(base + 1).fill(this.sequence[i]))));
+        }
+    }
+    return new PrSS(s.concat(new Array(base).fill(bad)));
+}
 }
 
 module.exports = PrSS;
